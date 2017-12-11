858ac385a84e8888b20c31bc05a6eaba593bf6b3
BorisPolonsky <qdogan@163.com>
2017-12-11 22:53:53 +0800

    Minor Update
    Tweaked:
    cpuSum => cpuArraySum
    gpuSum => gpuArraySum
    Added a paramter to specify the length of array in gpuArraySum.

--- 3 Array Sum/arraySum.cu
+++ 3 Array Sum/arraySum.cu
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 void setArray(float *, int);
-void cpuSum(float *, float *, float *, int);
+void cpuArraySum(float *, float *, float *, const int);
 void checkCpuMalloc(float *);
 void checkGpuMalloc(cudaError_t);
 int arrEqual(float*, float*, int);
-__global__ void gpuSum(float *, float *, float *);
+__global__ void gpuArraySum(float *, float *, float *, const int);
 
 int main()
 {
@@ -29,12 +29,12 @@ int main()
 	checkGpuMalloc(cudaMalloc((void**)&d_y, num * sizeof(float)));
 	cudaMemcpy(d_x1, h_x1, num * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_x2, h_x2, num * sizeof(float), cudaMemcpyHostToDevice);
-	cpuSum(h_x1, h_x2, h_y1, num);
+	cpuArraySum(h_x1, h_x2, h_y1, num);
 	//Perform array sum in GPU. 
 	dim3 block(2, 2, 2);
 	dim3 grid(2, 2, 2);
-	gpuSum<<<grid, block>>>(d_x1, d_x2, d_y);
-	cudaDeviceSynchronize(); //Wait for all kernels to complete. 
+	gpuArraySum<<<grid, block>>>(d_x1, d_x2, d_y, num);
+	cudaDeviceSynchronize(); //Explicitly wait for all kernels to complete. 
 	cudaMemcpy(h_y2, d_y, num * sizeof(float), cudaMemcpyDeviceToHost);
 	if(arrEqual(h_y1, h_y2, num))
 		printf("Result from GPU is equal to result from CPU. \n");
@@ -59,7 +59,7 @@ void setArray(float *p, int num)
 	}
 }
 
-void cpuSum(float *x1, float *x2, float *y, int num)
+void cpuArraySum(float *x1, float *x2, float *y, int num)
 {
 	for(int i = 0; i < num; i++)
 	{
@@ -89,11 +89,14 @@ void checkGpuMalloc(cudaError_t code)
 		exit(-1);
 }
 
-__global__ void gpuSum(float *x1, float*x2, float *y)
+__global__ void gpuArraySum(float *x1, float*x2, float *y, const int n)
 {
 	int threadOffset = threadIdx.x + threadIdx.y * blockDim.x + threadIdx.z * blockDim.x * blockDim.y; //thread in block
 	int blockOffset = blockIdx.x + blockIdx.y * gridDim.x + blockIdx.z * gridDim.x * gridDim.y; //block in grid 
 	int i = blockOffset * blockDim.x * blockDim.y * blockDim.z + threadOffset; 
 	printf("Adding entry no. %d in GPU\n", i);
-	y[i] = x1[i] + x2[i];
+	if(i < n)
+	{
+		y[i] = x1[i] + x2[i];
+	}
 }
