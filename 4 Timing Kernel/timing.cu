ab8801af7d392cca5b48f0fd44028caff7b8f4da
BorisPolonsky <qdogan@163.com>
2018-01-03 21:30:27 +0800

    Minor Update
    Remove gpuSum() (moved the corresponding code into the main function).
    Added equality check for two arrays.

--- 4 Timing Kernel/timing.cu
+++ 4 Timing Kernel/timing.cu
@@ -39,10 +39,38 @@ void checkGpuMalloc(cudaError_t code)
 		printf("CUDA ERROR occured. ");
 	}
 }
-double gpuSum(double *h_x1, double *h_x2, double *h_y, int arrSize)
+
+int checkEquality(double *arr1, double *arr2, const int arrSize)
 {
-	double *d_x1=NULL, *d_x2=NULL, *d_y=NULL;
+	for(int i=0; i< arrSize; i++)
+	{
+		if(arr1[i]!=arr2[i])
+			return 0;
+	}
+	return 1;
+}
+
+int main()
+{
+	int arrSize=1<<24; //16M
 	double tBegin, tEnd;
+	double *h_x1=NULL, *h_x2=NULL, *h_y1=NULL, *h_y2=NULL;
+	h_x1=(double*)malloc(arrSize*sizeof(double));
+	h_x2=(double*)malloc(arrSize*sizeof(double));
+	h_y1=(double*)malloc(arrSize*sizeof(double));
+	h_y2=(double*)malloc(arrSize*sizeof(double));
+	if(h_x1==NULL||h_x2==NULL||h_y1==NULL||h_y2==NULL)
+	{
+		printf("Failed to allocate enough memory\n");
+		exit(-1);
+	}
+	arrInit(h_x1, arrSize);
+	arrInit(h_x2, arrSize);
+	tBegin=cpuSecond();
+	cpuSum(h_x1, h_x2, h_y1, arrSize);
+	tEnd=cpuSecond();
+	printf("Time cost (CPU computation): %lfs\n", tEnd-tBegin);
+	double *d_x1=NULL, *d_x2=NULL, *d_y=NULL;
 	//Pre-allocate memory and check status.
 	checkGpuMalloc(cudaMalloc((void**)&d_x1, arrSize*sizeof(double)));
 	checkGpuMalloc(cudaMalloc((void**)&d_x2, arrSize*sizeof(double)));
@@ -52,35 +80,19 @@ double gpuSum(double *h_x1, double *h_x2, double *h_y, int arrSize)
 	tBegin=cpuSecond();
 	arraySumKernel<<<1,arrSize>>>(d_x1, d_x2, d_y, arrSize);//One block. 
 	tEnd=cpuSecond();
-	cudaMemcpy(h_y, d_y, arrSize*sizeof(double), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_y2, d_y, arrSize*sizeof(double), cudaMemcpyDeviceToHost);
 	cudaFree(d_x1);
 	cudaFree(d_x2);
 	cudaFree(d_y);
-	return (tEnd-tBegin);
-}
-int main()
-{
-	int arrSize=1<<24; //16M
-	double tBegin, tEnd;
-	double *h_x1=NULL, *h_x2=NULL, *h_y=NULL;
-	h_x1=(double*)malloc(arrSize*sizeof(double));
-	h_x2=(double*)malloc(arrSize*sizeof(double));
-	h_y=(double*)malloc(arrSize*sizeof(double));
-	if(h_x1==NULL||h_x2==NULL||h_y==NULL)
-	{
-		printf("Failed to allocate enough memory\n");
-		exit(-1);
-	}
-	arrInit(h_x1, arrSize);
-	arrInit(h_x2, arrSize);
-	tBegin=cpuSecond();
-	cpuSum(h_x1, h_x2, h_y, arrSize);
-	tEnd=cpuSecond();
-	printf("Time cost (CPU computation): %lfs\n", tEnd-tBegin);
-	printf("Time cost (GPU computation): %lfs\n", gpuSum(h_x1, h_x2, h_y, arrSize));
+	printf("Time cost (GPU computation): %lfs\n", tEnd-tBegin);
+	if(!checkEquality(h_y1, h_y2, arrSize))
+		printf("Arrays don't match\n");
+	else
+		printf("Arrays match.\n");
 	free(h_x1);
 	free(h_x2);
-	free(h_y);
+	free(h_y1);
+	free(h_y2);
 	cudaDeviceReset();
 	return 0;
 }
