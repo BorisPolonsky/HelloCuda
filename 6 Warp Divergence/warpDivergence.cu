e929b034f615b426cd11b0da151ac848b6ec9659
Polonsky <qdogan@163.com>
2018-01-10 20:28:25 +0800

    Minor Update
    Added kernel for warming up. (The purpose for this is to be figured out)
    Added exposedWarpDivergence(). (The difference between this kernel and simpleWarpDivergence() to be clarified and exposed)

--- 6 Warp Divergence/warpDivergence.cu
+++ 6 Warp Divergence/warpDivergence.cu
@@ -11,6 +11,20 @@ double cpuSecond()
 	return((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
 }
 
+__global__ void warmUp(double *arr, const int arrSize)
+{
+	//The same as minimalWarpDivergence()
+	int i=threadIdx.x+threadIdx.y*blockDim.x+threadIdx.z*blockDim.x*blockDim.y;
+	if(i<arrSize)
+	{	
+		if(TRUE)
+			arr[i]=i%2;
+		else
+			arr[i]=i%2;
+	}
+}
+
+
 __global__ void simpleWarpDivergence(double *arr, const int arrSize)
 {
 	int i=threadIdx.x+threadIdx.y*blockDim.x+threadIdx.z*blockDim.x*blockDim.y;
@@ -35,6 +49,19 @@ __global__ void minimalWarpDivergence(double *arr, const int arrSize)
 	}
 }
 
+__global__ void exposedWarpDivergence(double *arr, const int arrSize)
+{
+	int i=threadIdx.x+threadIdx.y*blockDim.x+threadIdx.z*blockDim.x*blockDim.y;
+	if(i<arrSize)
+	{	
+		bool flag=(i%2==0);
+		if(flag)
+			arr[i]=0;
+		else
+			arr[i]=1;
+	}
+}
+
 int main()
 {
 	const int arrSize=1<<16;
@@ -45,19 +72,35 @@ int main()
 		printf("Failed to allocated enough memory on GPU.\n");
 		exit(-1);
 	}
+	dim3 grid(1<<8), block(1<<8);
+
 	cudaMemset(d_arr, 0, arrSize*sizeof(double));
 	t=cpuSecond();
-	simpleWarpDivergence<<<1<<8, 1<<8>>>(d_arr,arrSize);
+	warmUp<<<grid, block>>>(d_arr,arrSize);
+	cudaDeviceSynchronize();
+	t=cpuSecond()-t;
+	printf("Warm up took %lf s.\n", t);
+
+	cudaMemset(d_arr, 0, arrSize*sizeof(double));
+	t=cpuSecond();
+	simpleWarpDivergence<<<grid, block>>>(d_arr,arrSize);
 	cudaDeviceSynchronize();
 	t=cpuSecond()-t;
 	printf("Array initialization with simple warp divergence took %lf s.\n", t);
 
 	cudaMemset(d_arr, 0, arrSize*sizeof(double));
 	t=cpuSecond();
-	minimalWarpDivergence<<<1<<8, 1<<8>>>(d_arr,arrSize);
+	minimalWarpDivergence<<<grid, block>>>(d_arr,arrSize);
 	cudaDeviceSynchronize();
 	t=cpuSecond()-t;
 	printf("Array initialization with minimal warp divergence took %lf s.\n", t);
+
+	cudaMemset(d_arr, 0, arrSize*sizeof(double));
+	t=cpuSecond();
+	exposedWarpDivergence<<<grid, block>>>(d_arr,arrSize);
+	cudaDeviceSynchronize();
+	t=cpuSecond()-t;
+	printf("Array initialization with exposed warp divergence took %lf s.\n", t);
 	cudaFree(d_arr);
 	cudaDeviceReset();
 	return 0;
